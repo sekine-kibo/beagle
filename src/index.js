942eafb07830d4d1f57011f589f62355440a9be4
sekine-kibo <Kibo.S.Hope@gmail.com>
2023-06-10 16:52:19 +0900

    install css-loader & style-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import sample from './modules/sample.js'
+import './modules/sample.css'
 
 console.log('Hello World!')
 sample()
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules:[
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          }
+        ]
+      }
+    ]
+  }
+}
